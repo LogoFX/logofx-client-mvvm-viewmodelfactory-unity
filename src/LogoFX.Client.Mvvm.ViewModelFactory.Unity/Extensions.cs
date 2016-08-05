107aa009b0b8cb198df5d26452f58b6a58bba98c
godrose <godrose@gmail.com>
2016-08-05 04:53:44 +0000

    1) Using ViewModelFactory package 2) United the projects 3) Updated assemblies' versions

---
+++ src/LogoFX.Client.Mvvm.ViewModelFactory.Unity/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using Solid.Bootstrapping;
+using Solid.Extensibility;
+
+namespace LogoFX.Client.Mvvm.ViewModelFactory.Unity
+{
+    /// <summary>
+    /// The bootstrapper extension methods.
+    /// </summary>
+    public static class BootstrapperExtensions
+    {
+        /// <summary>
+        /// Uses the view model factory which is based on LogoFX Simple Container.
+        /// </summary>        
+        /// <param name="bootstrapper">The bootstrapper.</param>
+        public static TBootstrapper UseViewModelFactory<TBootstrapper>(
+            this TBootstrapper bootstrapper)
+            where TBootstrapper : class, IExtensible<TBootstrapper>, IHaveContainerRegistrator
+        {
+            return bootstrapper.UseViewModelFactory<TBootstrapper, ViewModelFactory>();
+        }
+    }
+}
