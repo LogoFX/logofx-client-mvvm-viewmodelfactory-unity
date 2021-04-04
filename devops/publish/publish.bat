e9e00310e6126fc2539fb2fa815447f1b4924c21
Gennady Verdel <godrose@gmail.com>
2021-04-04 07:40:43 +0300

    devops: added means for package creation and storage #7

--- devops/publish/publish.bat
+++ devops/publish/publish.bat
@@ -1,6 +1,11 @@
+SET package_name=LogoFX.Client.Mvvm.ViewModelFactory.Unity
+SET package_version=2.2.0-rc2
 cd ../build
 call build.bat
 cd ../pack
 call ./pack.bat
 cd ../publish
-call ./copy.bat LogoFX.Client.Mvvm.ViewModelFactory.Unity 2.2.0-rc2 %1
+call ./copy.bat %package_name% %package_version% %1
+cd ../install
+call uninstall-global-single.bat %package_name% %package_version%
+cd ..
